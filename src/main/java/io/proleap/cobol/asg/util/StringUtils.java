bd3ef3f4313e8837cbe712f4b03d9e710f99197c
theHandZhang <2545874585@qq.com>
2023-09-06 01:25:11 +0800

    update String Utils and File Utils to adapt java8

--- src/main/java/io/proleap/cobol/asg/util/StringUtils.java
+++ src/main/java/io/proleap/cobol/asg/util/StringUtils.java
@@ -2,9 +2,50 @@ package io.proleap.cobol.asg.util;
 
 public class StringUtils {
 
+
+	public static String capitalize(final String str) {
+		if (str == null || str.isEmpty()) {
+			return str;
+		}
+
+		return str.substring(0, 1).toUpperCase() + str.substring(1);
+	}
+
+	public static int countMatches(final String str, final String target) {
+		return (str.length() - str.replace(target, "").length()) / target.length();
+	}
+
+	public static String leftPad(final String str, final int size) {
+		return leftPad(str, size, " ");
+	}
+
+	public static String leftPad(final String str, final int size, final String padChar) {
+		final int pads = Math.max(0, size - str.length());
+		return org.apache.commons.lang.StringUtils.repeat(padChar, pads).concat(str);
+	}
+
 	public static String lowercaseFirstLetter(final String data) {
 		final char firstLetter = Character.toLowerCase(data.substring(0, 1).charAt(0));
 		final String restLetters = data.substring(1);
 		return firstLetter + restLetters;
+	}
+
+	public static String rightPad(final String str, final int size) {
+		return rightPad(str, size, " ");
+	}
+
+	public static String rightPad(final String str, final int size, final String padChar) {
+		final int pads = Math.max(0, size - str.length());
+		return str.concat(org.apache.commons.lang.StringUtils.repeat(padChar, pads));
+	}
+
+	public static String substringAfter(final String str, final String separator) {
+		final int pos = str.indexOf(separator);
+		return pos == -1 ? "" : str.substring(pos + separator.length());
+	}
+
+	public static String substringBefore(final String str, final String separator) {
+		final int pos = str.indexOf(separator);
+		return pos == -1 ? str : str.substring(0, pos);
 	}
 }
